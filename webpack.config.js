57c92e5a5731c4cede965a5ba7fa70b471707eb2
Slav <shutrow@rambler.ru>
2019-10-23 01:50:50 +0300

    reverted back filepath for hashed img files in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -91,7 +91,7 @@ module.exports = (env, argv) => {
     output: {
       path: path.resolve(__dirname, "./dist"),
       filename: "[name].[hash].build.js",
-      publicPath: isProductionBuild ? "/ls-advanced-portfolio/" : "",
+      publicPath: isProductionBuild ? publicPath : "",
       chunkFilename: "[chunkhash].js"
     },
     module: {
