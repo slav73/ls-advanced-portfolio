945ce31ad1359c564dd721ed619114edb50641c1
Slav <shutrow@rambler.ru>
2019-10-29 13:34:49 +0300

    Skills block finished

--- src/scripts/skills-component.js
+++ src/scripts/skills-component.js
@@ -2,7 +2,23 @@ import Vue from "vue";
 
 const skill = {
   template: "#skill",
-  props: ["skillName", "skillPercent"]
+  props: ["skillName", "skillPercent"],
+  methods: {
+    drawColouredCircle() {
+      const circle = this.$refs["color-circle"];
+      console.log(circle.style);
+      const dashArray = parseInt(
+        getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+      );
+
+      const percent = (dashArray / 100) * (100 - this.skillPercent);
+
+      circle.style.strokeDashoffset = percent;
+    }
+  },
+  mounted() {
+    this.drawColouredCircle();    
+  }
 }
 
 const skillsRow = {
